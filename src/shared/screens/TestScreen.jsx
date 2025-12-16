f7d709ee915ae63f61ebea487eb96d45e1c7ab8e
Jonatan Santos <jonatan.vidal@alu.ufc.br>
2025-12-16 14:07:58 -0300

    Adiciona salvamento de dados

--- src/shared/screens/TestScreen.jsx
+++ src/shared/screens/TestScreen.jsx
@@ -1,10 +1,30 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Colors from "../constants/Colors";
 
 export default function TestScreen() {
   const insets = useSafeAreaInsets();
+
+  const [asyncStorageItemSet, setAsyncStorageItemSet] = useState(false);
+  const [user, setUser] = useState("");
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+
+  const storeData = (value) => {
+    AsyncStorage.setItem("user", JSON.stringify(value)).then((res) => {
+      setAsyncStorageItemSet(true);
+    });
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then((res) => {
+      setUser(res);
+    });
+  }, [asyncStorageItemSet]);
+
   return (
     <View
       style={{
@@ -25,7 +45,30 @@ export default function TestScreen() {
           borderTopRightRadius: 16,
         }}
       >
-        <Text>[ Teste ]</Text>
+        <View style={{ width: "80%", gap: 16 }}>
+          <TextInput
+            mode="outlined"
+            value={name}
+            cursorColor={Colors.Orange[800]}
+            activeOutlineColor={Colors.Orange[800]}
+            outlineStyle={{ borderRadius: 4, borderWidth: 0.5 }}
+            onChangeText={(text) => setName(text)}
+          />
+          <TextInput
+            mode="outlined"
+            value={age}
+            cursorColor={Colors.Orange[800]}
+            activeOutlineColor={Colors.Orange[800]}
+            outlineStyle={{ borderRadius: 4, borderWidth: 0.5 }}
+            onChangeText={(text) => setAge(text.replace(/[^0-9]/g, ""))}
+            keyboardType="numeric"
+          />
+        </View>
+        <Button mode="contained" onPress={() => storeData({ name, age })}>
+          Salvar
+        </Button>
+
+        <Text> {user} </Text>
       </View>
     </View>
   );
